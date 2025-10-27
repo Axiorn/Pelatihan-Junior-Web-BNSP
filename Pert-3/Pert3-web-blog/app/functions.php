d91818435187e88a9c127f031f6ce8e1df170502
latief <80023698+Axiorn@users.noreply.github.com>
2025-10-27 18:53:07 +0700

    Delete Pert-3 directory

--- Pert-3/Pert3-web-blog/app/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-// app/functions.php
-
-// escape helper untuk output aman
-if (!function_exists('e')) {
-    function e($str) {
-        return htmlspecialchars($str ?? '', ENT_QUOTES, 'UTF-8');
-    }
-}
